fcefbd75b3592bd5f52db984d22403ba233f924a
TARIQ <applandsys@gmail.com>
2026-04-16 22:39:20 +0600

    file name changed

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login } = require('../modules/auth/controller/AuthController.js');
 
 router.post('/register', register);
 router.post('/login', login);
